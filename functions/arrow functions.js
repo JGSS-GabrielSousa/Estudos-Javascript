7732af8bc5467127e811adbb09fbbbed3bc25769
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-03-10 13:52:36 -0300

    arrow functions

---
+++ functions/arrow functions.js
@@ -0,0 +1,13 @@
+function helloWorld1(){
+    return "Hello World";
+}
+
+const helloWorld2 = function(){
+    return "Hello World";
+}
+
+const helloWorld3 = () => {
+    "Hello World";
+}
+
+const helloWorld4 = () => "Hello World";
---
+++ functions/arrow functions2.js
@@ -0,0 +1,3 @@
+const soma = (a, b) => a + b;
+
+soma(4, 6);
